95a7fd135db93bc35cef32a8f9769ce213cf7339
DESKTOP-6SLRJDH\goget <Daniel Gans>
2024-04-02 17:54:38 +0200

    feat(main): phone and tel

--- src/components/nav/nav.component.ts
+++ src/components/nav/nav.component.ts
@@ -9,21 +9,22 @@ import { UsersService } from 'services/users.service';
   styleUrls: ['./nav.component.css'],
   imports: [ReactiveFormsModule],
   providers: [UsersService],
-  standalone: true
+  standalone: true,
 })
-export default class NavComponent implements OnDestroy{
-  subscription= new Subscription();
+export default class NavComponent implements OnDestroy {
+  subscription = new Subscription();
   userForm = new FormGroup({
     user: new FormControl(''),
   });
-  constructor(private usersService: UsersService) { 
-    this.subscription.add
-    (this.userForm.valueChanges.subscribe(({user}) => user && this.usersService.findUser(user)))
+  constructor(private usersService: UsersService) {
+    this.subscription.add(
+      this.userForm.valueChanges.subscribe(
+        ({ user }) => user && this.usersService.findUser(user)
+      )
+    );
   }
-  
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
-;
-
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-export class UsersService implements OnDestroy{
+export class UsersService implements OnDestroy {
   private users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
   subscription = new Subscription();
   url = 'https://jsonplaceholder.typicode.com/users';
@@ -15,21 +15,23 @@ export class UsersService implements OnDestroy{
   constructor(private http: HttpClient) {
     this.getUsersFromApi();
   }
-  
+
   getUsersFromApi(): void {
-    this.subscription.add(this.http.get<User[]>(this.url).subscribe((users) => {
-      this.users$.next(users);
-    }));
+    this.subscription.add(
+      this.http.get<User[]>(this.url).subscribe((users) => {
+        this.users$.next(users);
+      })
+    );
   }
-  
+
   get getUsers$(): Observable<User[]> {
     return this.users$.asObservable();
   }
-  
+
   findUser(search: string): void {
     this.filteredUsers = this.users$
-    .getValue()
-    .filter(({ name }) => name.toLowerCase().includes(search.toLowerCase()));
+      .getValue()
+      .filter(({ name }) => name.toLowerCase().includes(search.toLowerCase()));
     this.users$.next(this.filteredUsers);
   }
   ngOnDestroy(): void {
